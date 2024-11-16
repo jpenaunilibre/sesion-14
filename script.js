88aeeab859450d14e25eb1cb3b294eb9eb4e1347
Jandy <jandy.pena.ttc.506@unilibre.edu.co>
2024-11-15 21:26:52 -0500

    EJERCICIO 1 2 Y 3 DEL LABORATORIO 15

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.getElementById('nav-toggle'),addEventListener('click', function(){
     document.querySelector('nav').classList.toggle('nav-open');
 });
+
+function mostrarMensaje(texto){
+    alert(texto);
+}
